54e85db0e31fab4df5cac6c729c96a6a0a8265e2
depratt <127753172+depratt@users.noreply.github.com>
2023-05-26 09:35:28 -0400

    Add .js file

---
+++ RandomMessage.js
@@ -0,0 +1,15 @@
+ const quotes = [
+    {
+        quote: '"Do. Or do not. There is no try" -Yoda'
+    },
+    {
+        quote: '"Many of the truths that we cling to depend on our point of view." -Obi-Wan Kenobi'
+    },
+    {
+        quote: '"Your focus determines your reality." -Qui Gon Jinn'
+    },
+    {
+        quote: '"This is the way." -Din Djarin'
+    }
+];
+console.log(quotes[Math.floor(Math.random() * quotes.length)]);
